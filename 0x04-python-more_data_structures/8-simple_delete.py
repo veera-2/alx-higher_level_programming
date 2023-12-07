5230503183774e57c4e4afb24d25aeb3af141ccc
veera-2 <naveera.nh@gmail.com>
2023-12-07 01:20:30 +0100

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
-
-    for i in list_keys:
-        new_dir[i] *= 2
-
-    return (new_dir)
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
+    return (a_dictionary)
